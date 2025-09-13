baf06992e916cab00cf86c9972d891abda6c7f43
Hayden Pennington <haydenpennington@icloud.com>
2025-09-13 06:51:26 -0500

    Re init

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -40,6 +40,6 @@ testing {
 // Apply a specific Java toolchain to ease working on different environments.
 java {
     toolchain {
-        languageVersion = JavaLanguageVersion.of(21)
+        languageVersion = JavaLanguageVersion.of(9)
     }
 }
